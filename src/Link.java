efc89993bc6afad471d2c327429d0170bd4b6d3c
GrafVorontsov <2807LinuxForeverGH>
2024-01-14 15:02:34 +0200

    2 семестр 5й класс черновик

--- src/Link.java
+++ src/Link.java
@@ -3,21 +3,21 @@ public class Link {
     String matematika = "\"Идентификатор: 721 2764 7842" + System.lineSeparator() +
             "Код доступа: 123789" + System.lineSeparator() +
             "Ссылка: https://us04web.zoom.us/j/72127647842?pwd=W4O2LZ9WkadMaDXc6zqCY1qZNdgOPT.1\"";
-    String technology = "\"Идентификатор: 787 7208 6701" + System.lineSeparator() +
-            "Код доступа: 5uVzJB" + System.lineSeparator() +
-            "Ссылка: https://us04web.zoom.us/j/78772086701?pwd=kkY4CNaV8hLOOwkBBUxbTt7VlgJ5hM.1\"";
+    String technology = "\"Идентификатор: 717 0500 1714" + System.lineSeparator() +
+            "Код доступа: 2NXZDF" + System.lineSeparator() +
+            "Ссылка: https://us04web.zoom.us/j/71705001714?pwd=aDBcUuY98q1BUbIPptGNvpSr4bGsGz.1\"";
     String fizra = "\"Идентификатор: 899 4028 4581" + System.lineSeparator() +
             "Код доступа: T2U9c2" + System.lineSeparator() +
-            "Ссылка: https://us05web.zoom.us/j/89940284581?pwd=dbeoLb3MvNadxG3aa1fzJL3m8Q9Y3M.1 \"";
+            "Ссылка: https://us05web.zoom.us/j/89940284581?pwd=dbeoLb3MvNadxG3aa1fzJL3m8Q9Y3M.1\"";
     String priroda = "\"Идентификатор: 793 5335 7220" + System.lineSeparator() +
             "Код доступа: 12345" + System.lineSeparator() +
             "Ссылка: https://us04web.zoom.us/j/79353357220?pwd=gwbPj6Yub7PqaPoCu4AbPNhmXENxR5.1\"";
     String music = "\"Идентификатор: 507 995 0701" + System.lineSeparator() +
             "Код доступа: 1rJ3Di" + System.lineSeparator() +
             "Ссылка: https://us05web.zoom.us/j/5079950701?pwd=MjBXbmtyNFZUZHlaZlZFeS9STzJzUT09\"";
-    String healthy = "\"Идентификатор: 734 3878 3352" + System.lineSeparator() +
-            "Код доступа: 00Rc8w" + System.lineSeparator() +
-            "Ссылка: https://us04web.zoom.us/j/73438783352?pwd=euov3djO8cbZwwFhzB8CkxPcJ3QQJ3.1\"";
+    String healthy = "\"Идентификатор: 747 8407 4503" + System.lineSeparator() +
+            "Код доступа: 6jiYhV" + System.lineSeparator() +
+            "Ссылка: https://us04web.zoom.us/j/74784074503?pwd=bPZ5BWTnioybqCQIVBkzUOqzdOH3Wv.1\"";
     String informatika = "\"Идентификатор: 731 9568 3969" + System.lineSeparator() +
             "Код доступа: 123321" + System.lineSeparator() +
             "Ссылка: https://us04web.zoom.us/j/73195683969?pwd=JDifQm6dsoErUoVikkGPFnxdxOx393.1\"";
@@ -46,21 +46,22 @@ public class Link {
 
 /*
 Музичне мистецтво – Охрій Олександр Анатолійович
-Технології – Пономаренко Ірина Олексіївна
-Пізнаємо природу – Бондар Катерина Олександрівна
-Англійська мова – Жеребілова Олена Сергіївна
-Фізична культура – Плужніков Антон Олексійович
-Здоров'я, безпека та добробут – Лютенко Анастасія Григорівна
 
+********* Чёрный список *********
 Історія – Плутахіна Тетяна Ігорівна
 Образотворче мистецтво – Мироненко Світлана Ігорівна
+*********************************
 
-----------------------------------------------------
+------------ Адекватные -------------------
 
 Українська мова – Можин Володимир Миколайович
 Українська література – Можин Володимир Миколайович
 Інформатика - Ремньова Анастасія Олексіївна
 Математика - Ремньова Анастасія Олексіївна
 Зарубіжна література – Кондратенко Ганна Анатоліївна
-
+Фізична культура – Плужніков Антон Олексійович
+Пізнаємо природу – Бондар Катерина Олександрівна
+Англійська мова – Жеребілова Олена Сергіївна
+Здоров'я, безпека та добробут – Лютенко Анастасія Григорівна
+Технології – Пономаренко Ірина Олексіївна
  */
--- src/Timing.java
+++ src/Timing.java
@@ -1,39 +1,26 @@
 public class Timing {
     //расписание звонков
+
     //модуль 1
     String _1Start915 = "9:15:00";
     String _1Start900 = "9:00:00";
-    //String _1Finish = "9:45:00";
-
-    //String _2Start955 = "9:55:00";
-    //String _2Start = "10:00:00";
     String _1Finish = "10:40:00";
 
     //модуль 2
     String _2Start1100 = "11:00:00";
     String _2Start1050 = "10:50:00";
     String _2Start = "10:45:00";
-    //String _3Finish = "11:35:00";
-
-    //String _4Start1145 = "11:45:00";
-    //String _4Start = "12:00:00";
     String _2Finish = "12:30:00";
 
     //модуль 3
     String _3Start1250 = "12:50:00";
     String _3Start1245 = "12:45:00";
     String _3Start = "13:00:00";
-    //String _3Finish = "13:35:00";
-
-    //String _6Start = "13:45:00";
     String _3Finish = "14:30:00";
 
     //модуль 4
     String _4Start1445 = "14:45:00";
     String _4Start1450 = "14:50:00";
     String _4Start = "15:00:00";
-    //String _7Finish = "15:35:00";
-
-    //String _8Start = "15:45:00";
     String _4Finish = "16:30:00";
 }
